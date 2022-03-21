ad4ff49c975f5299a35312f8d129e36fa4b96384
0xol <86998622+0xol@users.noreply.github.com>
2022-03-21 22:44:34 +1000

    ello varibale

---
+++ 3-ello variable/main.py
@@ -0,0 +1,12 @@
+
+number1 = 1
+number2 = 2
+number3 = number1 + number2
+string = "this is a string"
+string2 = "string and a number " + str(number3) # if you dont specift that number3 is a string, it will through an error because string isnt a number
+
+#print (variable)
+print(string)
+print(number1)
+print(number3)
+print(string2)
---
+++ 3-ello variable/readme.md
@@ -0,0 +1,10 @@
+# Ello Variable
+
+to run this program type this line in a terminal located in this directory.  
+```
+build
+```
+or.  
+```
+python main.py
+```
