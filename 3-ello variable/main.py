45bea556ca0c8f45b8d345dd98c6d8be3514ae9a
0xol <86998622+0xol@users.noreply.github.com>
2022-04-19 23:22:46 +1000

    Update main.py

--- 3-ello variable/main.py
+++ 3-ello variable/main.py
@@ -3,7 +3,7 @@ number1 = 1
 number2 = 2
 number3 = number1 + number2
 string = "this is a string"
-string2 = "string and a number " + str(number3) # if you dont specift that number3 is a string, it will through an error because string isnt a number
+string2 = "string and a number " + str(number3) # if you dont specify that number3 is a string, it will through an error because it tries to do 3 + "string and a number"
 
 #print (variable)
 print(string)
